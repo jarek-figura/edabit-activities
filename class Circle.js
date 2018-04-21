976eb1680f6230f624bd3755ef75110e385b5318
jarek-figura <jarek.figura@icloud.com>
2018-04-21 12:01:05 +0200

    class Circle

---
+++ class Circle.js
@@ -0,0 +1,21 @@
+class Rectangle {
+	constructor(sideA,sideB){
+		this.sideA = sideA;
+		this.sideB = sideB;
+	}
+	getArea(){return this.sideA*this.sideB};
+	getPerimeter(){return (this.sideA + this.sideB) *2 };
+}
+
+class Circle {
+	constructor(radius) {
+		this.radius = radius;
+	}
+	getArea() { return Math.PI * this.radius * this.radius };
+	getPerimeter() { return 2 * Math.PI * this.radius };
+}
+
+//unquote and use run to test these cases
+let q = new Circle(4.44);
+console.log(q.getArea());
+console.log(q.getPerimeter());
